7cbb704ede9879faa6f6ed0164dab728e6166efe
kalliopi <koutsiouki.kal@gmail.com>
2024-01-26 21:49:40 +0200

    initial commit

---
+++ src/main/java/com/papei/movie_ticket_reservations/MovieTicketReservationsApplication.java
@@ -0,0 +1,13 @@
+package com.papei.movie_ticket_reservations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieTicketReservationsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieTicketReservationsApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/papei/movie_ticket_reservations/model/User.java
@@ -0,0 +1,6 @@
+package com.papei.movie_ticket_reservations.model;
+
+public class User {
+
+}
+git
